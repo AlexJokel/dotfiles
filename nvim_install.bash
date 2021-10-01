0284c366a811779f35f616b66cdd9dbb51259a80
Alexey Bakevich <bakevich@list.ru>
2021-10-01 21:58:09 +0300

    Scriptify nvim install

--- nvim_install.bash
+++ nvim_install.bash
@@ -7,11 +7,12 @@ mkdir -p "$BIN_PATH"
 cd "$BIN_PATH"
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
-ln -s nvim.appimage nvim
-ln -s nvim vim
+ln -sf nvim.appimage nvim
+ln -sf nvim vim
 nvim -c "PlugInstall|qa"
-nvim -c "Tmuxline|TmuxlineSnapshot ~/.vim/tmuxline|qa"
-nvim -c "LspInstall cpp" -c "LspInstall python" -c "LspInstall vim" -c "LspInstall lua" -c "LspInstall bash"
+if [[ -n "$TMUX" ]]; then
+    nvim -c "Tmuxline|TmuxlineSnapshot ~/.vim/tmuxline|qa"
+fi
 if ! grep -q "PATH=.*\.local/bin" ~/.bashrc; then
     echo -e "export PATH=\"$BIN_PATH:\$PATH\"\n" >> ~/.bashrc
     echo "Restart shell or reload config file to access nvim."
@@ -28,4 +29,4 @@ if ! grep -qxF "export BAT_THEME=\"OneHalfLight\"" ~/.bashrc; then
     echo -e "export BAT_THEME=\"OneHalfLight\"\n" >> ~/.bashrc
 fi
 cd "$BIN_PATH"
-ln -s /usr/bin/batcat bat
+ln -sf /usr/bin/batcat bat
