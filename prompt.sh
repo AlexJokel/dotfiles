d11ad6001633be32ccf9e69d77d5897cfe41cbf2
Alex Bakevich <alexey.bakevich@gmail.com>
2025-03-05 09:14:32 +0000

    Use rounded separator

--- prompt.sh
+++ prompt.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
 __my_prompt() {
-  local BLACK_FG="$(tput setaf 0)"
-  local WHITE_FG="$(tput setaf 231)"
-  local WHITE_BG="$(tput setab 231)"
-  local  GRAY_FG="$(tput setaf 255)"
-  local  GRAY_BG="$(tput setab 255)"
-  local GREEN_BG="$(tput setab 156)"
-  local GREEN_FG="$(tput setaf 156)"
-  local  BLUE_BG="$(tput setab 153)"
-  local  BLUE_FG="$(tput setaf 153)"
-  local  PINK_BG="$(tput setab 224)"
-  local  PINK_FG="$(tput setaf 224)"
-  local    RESET="$(tput sgr0)"
+  local  BLACK_FG="$(tput setaf 0)"
+  local   BLUE_BG="$(tput setab 153)"
+  local   BLUE_FG="$(tput setaf 153)"
+  local  GREEN_BG="$(tput setab 156)"
+  local  GREEN_FG="$(tput setaf 156)"
+  local   PINK_BG="$(tput setab 224)"
+  local   PINK_FG="$(tput setaf 224)"
+  local  WHITE_FG="$(tput setaf 231)"
+  local  WHITE_BG="$(tput setab 231)"
+  local   GRAY_FG="$(tput setaf 255)"
+  local   GRAY_BG="$(tput setab 255)"
+  local     RESET="$(tput sgr0)"
+  local   REVERSE="$(tput rev)"
+
+  # local  left_sep=""
+  # local right_sep=""
+  local  left_sep=""
+  local right_sep=""
 
   prompt_error_segment() {
     if [ "$1" = 0 ]; then
@@ -23,13 +29,15 @@ __my_prompt() {
   }
 
   right_side="\
-$GREEN_FG$BLACK_FG$GREEN_BG ${debian_chroot:+($debian_chroot)}\u@\h $GREEN_BG\
-$BLUE_FG$BLACK_FG$BLUE_BG \d \A $RESET$BLUE_FG"
-  right_side_non_printable_count=59
+$GREEN_FG$right_sep$BLACK_FG$GREEN_BG ${debian_chroot:+($debian_chroot)}\u@\h $GREEN_BG\
+$BLUE_FG$right_sep$BLACK_FG$BLUE_BG \d \A $RESET"
+  right_side_non_printable_count=48
 
   PS1="\
 $(tput sc; printf "%*s" $(($(tput cols) + right_side_non_printable_count)) "$right_side"; tput rc)\
 $(prompt_error_segment "$1")\
 $GRAY_BG$BLACK_FG \w $RESET$GRAY_FG\
 $RESET\n \$ "
+
+  unset -f prompt_error_segment
 }
